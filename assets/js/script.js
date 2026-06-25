48544a57d15bbe87c4a5af84bb611eaf4e084c4f
SucitaAprilliany <146791610+SucitaAprilliany@users.noreply.github.com>
2026-06-25 11:06:44 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,11 +59,11 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | CodeWithTanveer";
+            document.title = "Portfolio | SUCITA";
             $("#favicon").attr("href", "assets/images/favicon.jpeg");
         }
         else {
-            document.title = "Portfolio | Tanveer Ahmad";
+            document.title = "Portfolio | SUCITA";
             $("#favicon").attr("href", "assets/images/favicon.jpeg");
         }
     });
